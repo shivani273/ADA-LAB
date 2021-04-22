c20c5c9849c161e80dd7b562a17d091ca6a1204d
shivani273 <71484984+shivani273@users.noreply.github.com>
2021-04-22 12:58:22 +0530

    Add files via upload

---
+++ LAB PROGRAM 2/SELECTION SORT.C
@@ -0,0 +1,52 @@
+#include <stdio.h>
+  
+void swap(int *xp, int *yp)
+{
+    int temp = *xp;
+    *xp = *yp;
+    *yp = temp;
+}
+  
+void selectionsort(int list[], int size)
+{
+    int i, j, min;
+  
+    // One by one move boundary of unsorted subarray
+    for (i = 0; i < size-1; i++)
+    {
+        // Find the minimum element in unsorted array
+        min = i;
+        for (j = i+1; j < size; j++)
+          if (list[j] < list[min])
+            min = j;
+  
+        // Swap the found minimum element with the first element
+        swap(&list[min], &list[i]);
+    }
+}
+  
+/* Function to print an array */
+void printArray(int list[], int size)
+{
+    int i;
+    for (i=0; i < size; i++)
+        printf("%d ", list[i]);
+    printf("\n");
+}
+  
+// Driver program to test above functions
+int main()
+{
+    int size, list[25];
+    printf("Enter the size of the list: ");
+    scanf("%d",&size);
+    printf("Enter the elements of the list: ");
+    for(int i=0;i<size;i++)
+    {
+        scanf("%d",&list[i]);
+    }
+    selectionsort(list, size);
+    printf("After selection sort: \n");
+    printArray(list, size);
+    return 0;
+}
